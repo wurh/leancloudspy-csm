81754b96290e2a4ac0584f024ad6ec4a88869bef
wurh <wurh@users.noreply.github.com>
2016-03-01 10:13:43 +0800

    [UPD] initial

---
+++ src/init.js
@@ -0,0 +1,7 @@
+
+var AV = require('leanengine')
+var APP_ID = 'cvrivo7vfxzyfi6qd9g6hmzfb6wvn6xuvw7luybud02ppmnp'		// your app id
+var APP_KEY = 'l2heqwn47xxiaj77pjsup2w9rtoj62yaxos2peukutva8a0e'	// your app key
+var MASTER_KEY = '09xc3jyn7v2kd7pt3614gv0ygp3jqka3naaxf8qsbl8t4x6a' // your app master key
+
+AV.initialize(APP_ID, APP_KEY, MASTER_KEY);
